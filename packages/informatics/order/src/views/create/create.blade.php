8cdf975aa8d5e43c87a24a1cbfcbe15f94e9e1df
socloccoc <daihusk57@gmail.com>
2019-10-18 14:28:10 +0700

    fix bug

--- packages/informatics/order/src/views/create/create.blade.php
+++ packages/informatics/order/src/views/create/create.blade.php
@@ -20,9 +20,13 @@
                     <li class="active"><a href="#tab_1" data-toggle="tab">Thông tin chi tiết</a></li>
                 </ul>
                 <div class="tab-content">
-                    <form class="form-horizontal" method="POST" action="{{ route('order.store') }}">
+                    <form class="form-horizontal" method="POST" action="{{ route('order.store') }}" novalidate
+                          autocomplete="off">
                         <div class="parent-border col-sm-12">
                             <div class="form-group">
+                                <div class="col-sm-1">
+                                    <button type="button" class="btn btn-primary count btn-circle btn-lg">1</button>
+                                </div>
                                 <div class="col-sm-offset-11">
                                     <button type="button" id="deleteRow" class="btn btn-danger btn-circle btn-lg"><i
                                                 class="fa fa-trash"></i></button>
@@ -30,17 +34,19 @@
                             </div>
                             <div class="col-sm-4">
                                 <label class="control-label" for="full_name">Họ và tên:</label>
-                                <input type="text" class="form-control" name="full_name[]" placeholder="Họ và tên">
+                                <input type="text" class="form-control" required maxlength="191" name="full_name[]"
+                                       placeholder="Họ và tên">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="phone">SĐT:</label>
-                                <input type="text" class="form-control" name="phone[]" placeholder="0987654321">
+                                <input type="tel" class="form-control" required maxlength="20" name="phone[]"
+                                       placeholder="0987654321">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="province">Tỉnh :</label>
-                                <select class="form-control province" name="province[]">
+                                <select class="form-control province" name="province[]" required>
                                     <option value="">--- Tỉnh, thành phố ---</option>
                                     @forelse($province as $item)
                                         <option value="{{ $item['province'] }}">{{ $item['province'] }}</option>
@@ -51,7 +57,7 @@
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="district">Huyện :</label>
-                                <select class="form-control district" name="district[]">
+                                <select class="form-control district" name="district[]" required>
                                     <option value="">--- Huyện ---</option>
                                 </select>
                             </div>
@@ -64,47 +70,47 @@
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="street">Đường:</label>
-                                <input type="text" class="form-control" name="street[]">
+                                <input required maxlength="191" type="text" class="form-control" name="street[]">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="store_name">Tên store:</label>
-                                <input type="text" class="form-control" name="store_name[]">
+                                <input required maxlength="191" type="text" class="form-control" name="store_name[]">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="product_name">Tên sản phẩm:</label>
-                                <input type="text" class="form-control" name="product_name[]">
+                                <input required maxlength="191" type="text" class="form-control" name="product_name[]">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="product_link">Đơn hàng/link sp:</label>
-                                <input type="text" class="form-control" name="product_link[]">
+                                <input maxlength="191" type="text" class="form-control" name="product_link[]">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="quantity">Số lượng:</label>
-                                <input type="text" class="form-control" name="quantity[]">
+                                <input required type="number" class="form-control" name="quantity[]">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="option_1">Lựa chọn 1:</label>
-                                <input type="text" class="form-control" name="option_1[]">
+                                <input required maxlength="191" type="text" class="form-control" name="option_1[]">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="option_2">Lựa chọn 2:</label>
-                                <input type="text" class="form-control" name="option_2[]">
+                                <input required maxlength="191" type="text" class="form-control" name="option_2[]">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="promo_code">Promo code:</label>
-                                <input type="text" class="form-control" name="promo_code[]">
+                                <input maxlength="191" type="text" class="form-control" name="promo_code[]">
                             </div>
 
                             <div class="col-sm-4">
                                 <label class="control-label" for="transport">Vận chuyển:</label>
-                                <input type="text" class="form-control" name="transport[]">
+                                <input maxlength="191" required type="text" class="form-control" name="transport[]">
                             </div>
                         </div>
                         <div id="append-container">
@@ -120,7 +126,9 @@
                             <div class="navbar-footer pull-right">
                                 <ul class="nav navbar-nav">
                                     <li>
-                                        <button type="submit" class="btn btn-lg btn-success">Save</button>
+                                        <button onclick="confirm('Bạn chắc chắn muốn lưu và xuất file không ?');"
+                                                type="submit" class="btn btn-lg btn-success">Save
+                                        </button>
                                     </li>
                                 </ul>
                             </div>
@@ -136,6 +144,14 @@
 @endsection
 
 <script>
+    $(window).load(function () {
+        var bs = new base({
+            forms: [{
+                validate: true,
+                $obj: $('form')
+            }]
+        });
+    });
     $(document).ready(function () {
         initAddress('.parent-border');
 
@@ -167,7 +183,7 @@
                                         data: {district: district, province: province},
                                         dataType: 'JSON',
                                         success: function (data) {
-                                            district_eml.parents(parentClass).find('.village').find('option').not(':first').remove();
+                                            district_eml.parents(parentClass).find('.village').find('option').remove();
                                             $.each(data, function (i, item) {
                                                 district_eml.parents(parentClass).find('.village').append($('<option>', {
                                                     value: item.village,
@@ -184,13 +200,18 @@
             });
         }
 
-        $('#deleteRow').closest('.form-group').hide();
+        $('#deleteRow').hide();
+        // $('.count').parents().show();
         $('#addRow').on('click', function (e) {
+            var len = $('.child-border').length;
             $('.parent-border').clone().find(':input').each(function (idx, ele) {
                 ele.value = '';
-            }).end().find('.form-group').toggle(true).end()
+            }).end().find('#deleteRow').show().end().find('.form-group').toggle(true).end()
                 .toggleClass('parent-border child-border').hide()
                 .appendTo('#append-container').slideDown('slow');
+            $('.child-border').each(function (idx, ele) {
+                $(this).find('.count').html(idx + 2).css('margin-left', '10px');
+            });
             initAddress('.child-border');
         });
 
@@ -202,6 +223,9 @@
                     return acc;
                 }, {});
             $(this).closest('.child-border, .parent-border').remove();
+            $('.child-border').each(function (idx, ele) {
+                $(this).find('.count').html(idx + 2).css('margin-left', '10px');
+            });
         });
 
     });
